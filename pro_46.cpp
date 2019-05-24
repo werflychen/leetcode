e27eb68f321864ddea3deaef8efe7e3c04961038
werflychen <chenwhdream@gmail.com>
2019-05-24 20:48:00 +0800

    Create pro_46.cpp

---
+++ pro_46.cpp
@@ -0,0 +1,69 @@
+//关键点：问题可以被描述为一个更小规模的一个子问题，所以可以采用递归解决问题
+//全排列问题：描述为每次从数组中取一个数，该数与剩下的子数组的组成的全排列，即是问题的解
+
+class Solution {
+public:
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        if (nums.size() == 0) {
+            return result;
+        }    
+        
+        do_permute(nums, 0, result);
+        return result;
+    }
+    
+    void do_permute(vector<int>& nums, int cur, vector<vector<int>>& result) {
+        if (cur == nums.size() - 1) {
+            result.push_back(nums);
+            return;
+        }
+        
+        for (size_t i = cur; i < nums.size(); ++i) {
+            std::swap(nums[i], nums[cur]);
+            do_permute(nums, cur + 1, result);
+            std::swap(nums[i], nums[cur]);
+        }
+    }
+};
+
+
+
+//变种题：如果数组存在重复，则每次替换过的元素，不用再进行替换，用个map记录
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        if (nums.size() == 0) {
+            return result;
+        }
+        
+        do_permute(nums, 0, result);
+        
+        return result;
+    }
+    
+    
+    void do_permute(vector<int>& nums, int cur, vector<vector<int>>& result) {
+        if (cur == nums.size() - 1) {
+            result.push_back(nums);
+            return;
+        }
+        
+        map<int, bool> m_tmp;
+        for (size_t i = cur; i < nums.size(); ++i) {
+            if (m_tmp.find(nums[i]) != m_tmp.end()) {
+                    continue;
+            }
+            m_tmp[nums[i]] = true;
+            
+            std::swap(nums[i], nums[cur]);
+            do_permute(nums, cur + 1, result);
+            std::swap(nums[i], nums[cur]);
+        }
+    }
+};
+
+
+//非递归算法，采用字典序的解法，待补充
