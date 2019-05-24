4a766749040c573d15d19466b8474f865afb153c
werflychen <chenwhdream@gmail.com>
2019-05-24 20:49:24 +0800

    Update pro_46.cpp

--- pro_46.cpp
+++ pro_46.cpp
@@ -30,40 +30,4 @@ public:
 
 
 
-//变种题：如果数组存在重复，则每次替换过的元素，不用再进行替换，用个map记录
-class Solution {
-public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> result;
-        if (nums.size() == 0) {
-            return result;
-        }
-        
-        do_permute(nums, 0, result);
-        
-        return result;
-    }
-    
-    
-    void do_permute(vector<int>& nums, int cur, vector<vector<int>>& result) {
-        if (cur == nums.size() - 1) {
-            result.push_back(nums);
-            return;
-        }
-        
-        map<int, bool> m_tmp;
-        for (size_t i = cur; i < nums.size(); ++i) {
-            if (m_tmp.find(nums[i]) != m_tmp.end()) {
-                    continue;
-            }
-            m_tmp[nums[i]] = true;
-            
-            std::swap(nums[i], nums[cur]);
-            do_permute(nums, cur + 1, result);
-            std::swap(nums[i], nums[cur]);
-        }
-    }
-};
-
-
 //非递归算法，采用字典序的解法，待补充
